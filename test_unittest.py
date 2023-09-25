87ab837013c4eb3657a0c7db4e5e58f20f6244c0
Simranjit Singh Babbar <111028218+JeeT001@users.noreply.github.com>
2023-09-25 18:59:00 +1300

    Added Comments and Docstrings

--- test_unittest.py
+++ test_unittest.py
@@ -6,17 +6,21 @@ from BowlingGame import BowlingGame  # Assuming BowlingGame is defined in Bowlin
 class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
+        # Create a new instance of BowlingGame before each test
         self.game = BowlingGame()
 
     def testGutterGame(self):
+        # Test a game where all rolls result in 0 pins knocked down (a "gutter game").
         self.rollMany(0, 20)
         self.assertEqual(self.game.score(), 0)
         
     def testAllOnes(self):
+         # Test a game where all rolls result in 1 pin knocked down.
         self.rollMany(1, 20)
         self.assertEqual(self.game.score(), 20)
         
     def testOneSpare(self):
+        # Test a game with one spare (5 pins in the first frame, 3 in the next).
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
@@ -24,6 +28,7 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(self.game.score(), 16)
         
     def testOneStrike(self):
+        # Test a game with one strike (all pins knocked down in the first frame).
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
@@ -31,14 +36,17 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(self.game.score(), 24)
         
     def testPerfectGame(self):
+        # Test a perfect game with all strikes (12 strikes for 300 points).
         self.rollMany(10,12)
         self.assertEqual(self.game.score(), 300)
         
     def testAllFives(self):
+        # Test a game where all rolls result in 5 pins knocked down (a score of 150).
         self.rollMany(5,21)
         self.assertEqual(self.game.score(), 150)
         
     def rollMany(self, pins,rolls):
+        # Helper function to roll the same number of pins multiple times.
         for _ in range(rolls):
             self.game.roll(pins)
             
